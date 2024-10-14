4c06dd24a1584e66171cb24207e798755de26f4e
Patrick von Steht <4501324-patrick-vonsteht@users.noreply.gitlab.com>
2024-10-14 14:46:34 +0200

    Fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/frontendmentor-product-preview-card-component/" : "/",
   plugins: [
     vue(),
   ],
