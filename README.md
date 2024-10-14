03239100b3b0123c3b6850503cbd94e99c3b1e01
Patrick von Steht <4501324-patrick-vonsteht@users.noreply.gitlab.com>
2024-10-14 14:14:05 +0200

    Add solution

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontend Mentor - FAQ accordion solution
+This is a solution to the [Product preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa). 
+
+## Solution Approach
+* Vue.js (Composition API, Single File Components, Internal CSS)
+* NPM 
+* Javascript Modules
+
+## Solution Screenshots
+### Mobile Solution
+![Mobile Result](/result/mobile_result.png?raw=true "Mobile Result")
+### Desktop Solution
+![Desktop Result](/result/desktop_result.png?raw=true "Desktop Result")
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="card">
+      <img class="card-img" alt=""></img>
+      <div class="card-content">
+        <p class="card-category">Perfume</p>
+        <h1 class="card-title">Gabrielle Essence Eau De Parfum</h1>
+        <p class="card-description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+        <div class="card-price">
+          <p class="card-price-current">$149.99</p>
+          <p class="card-price-previous">$169.99</p>
+        </div>
+        <a class="card-buybtn" href="#"><img class="card-buybtn-img" src="./assets/icon-cart.svg" alt=""><span class="card-buybtn-text">Add to Cart</span></a>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Patrick von Steht</a>.
+    </div>
+  </footer>
+</template>
